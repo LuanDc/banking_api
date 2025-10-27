d191755436cdd35359798b78db9a4bd79a61183a
luan fellipe <luan.fellipe@live.com>
2025-10-27 19:01:19 +0100

    chore: create new phoenix app

---
+++ test/banking_api_web/controllers/page_controller_test.exs
@@ -0,0 +1,8 @@
+defmodule BankingApiWeb.PageControllerTest do
+  use BankingApiWeb.ConnCase
+
+  test "GET /", %{conn: conn} do
+    conn = get(conn, ~p"/")
+    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
+  end
+end
