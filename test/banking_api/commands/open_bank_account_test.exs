3c4a1ea51aeb886197535e01779b0b10a9d08711
luan fellipe <luan.fellipe@live.com>
2025-11-17 21:27:58 +0100

    refact: move validation tests to command module

--- test/banking_api/commands/open_bank_account_test.exs
+++ test/banking_api/commands/open_bank_account_test.exs
@@ -18,4 +18,31 @@ defmodule BankingApi.BankAccounts.Commands.OpenBankAccountTest do
       assert bank_account.id == id
     end
   end
+
+  describe "valid?/1" do
+    @open_bank_account_command %OpenBankAccount{
+      initial_balance: 1000,
+      account_number: "1234567890",
+      status: "open"
+    }
+
+    @required_params [:initial_balance, :account_number, :status]
+
+    for required_param <- @required_params do
+      test "returns an error with the reason when #{required_param} param is not given" do
+        param = Map.new() |> Map.put_new(unquote(required_param), nil)
+        open_bank_account_command = struct(@open_bank_account_command, param)
+        refute OpenBankAccount.valid?(open_bank_account_command)
+      end
+    end
+  end
+
+  test "returns an error when the given is status is not open or closed" do
+    open_bank_account_command = %OpenBankAccount{
+      @open_bank_account_command
+      | status: "invalid_status"
+    }
+
+    refute OpenBankAccount.valid?(open_bank_account_command)
+  end
 end
