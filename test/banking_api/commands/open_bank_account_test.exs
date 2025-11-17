6291fe02e40cbec8ef991426ad57488e1a26aa1c
luan fellipe <luan.fellipe@live.com>
2025-11-17 20:43:39 +0100

    test: returns a OpenBankAccount command struct with the given initial values

--- test/banking_api/commands/open_bank_account_test.exs
+++ test/banking_api/commands/open_bank_account_test.exs
@@ -3,19 +3,11 @@ defmodule BankingApi.BankAccounts.Commands.OpenBankAccountTest do
 
   alias BankingApi.BankAccounts.Commands.OpenBankAccount
 
-  @params %{
-    "initial_balance" => 1000,
-    "account_number" => "1234567890",
-    "status" => "open"
-  }
-
   describe "new/1" do
-    test "returns a OpenBankAccount command struct when the given params are valid" do
-      bank_account = OpenBankAccount.new(@params)
-      assert %OpenBankAccount{} = bank_account
-      assert bank_account.initial_balance == @params["initial_balance"]
-      assert bank_account.account_number == @params["account_number"]
-      assert bank_account.status == @params["status"]
+    test "returns a OpenBankAccount command struct with the given initial values" do
+      params = %{"initial_balance" => 1000}
+      bank_account = OpenBankAccount.new(params)
+      assert %OpenBankAccount{initial_balance: 1000} = bank_account
     end
   end
 
