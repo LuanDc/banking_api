2d9f0fffdf68c35041640e7a0510b021495561af
luan fellipe <luan.fellipe@live.com>
2025-11-21 19:06:46 +0100

    test: make sure aggregate state are what we wanted

--- test/banking_api/aggregates/bank_account_test.exs
+++ test/banking_api/aggregates/bank_account_test.exs
@@ -3,11 +3,12 @@ defmodule BankingApi.Aggregates.BankAccountTest do
 
   import Commanded.Assertions.EventAssertions
 
+  alias BankingApi.BankAccounts.Aggregates.BankAccount
   alias BankingApi.BankingApiApp
   alias BankingApi.BankAccounts.Events.BankAccountOpened
 
   describe "OpenBankAccount" do
-    test "ensure any event of BankAccountOpened type is published" do
+    test "make sure any event of BankAccountOpened type is published" do
       command = build_command(:open_bank_account)
       assert :ok = BankingApiApp.dispatch(command)
 
@@ -15,6 +16,18 @@ defmodule BankingApi.Aggregates.BankAccountTest do
         assert event.account_number == command.account_number
         assert event.initial_balance == command.initial_balance
         assert event.status == "open"
+      end)
+    end
+
+    test "make sure aggregate state are what we wanted" do
+      command = build_command(:open_bank_account)
+      assert :ok = BankingApiApp.dispatch(command)
+
+      wait_for_event(BankingApiApp, BankAccountOpened, fn event ->
+        bank_account = BankAccount.apply(%BankAccount{}, event)
+        assert bank_account.id == command.id
+        assert bank_account.account_number == command.account_number
+        assert bank_account.balance == command.initial_balance
       end)
     end
 
