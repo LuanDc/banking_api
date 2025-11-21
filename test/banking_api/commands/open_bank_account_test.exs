bc4899468f5e266e339eff6e1c97aa2d9a193c5a
luan fellipe <luan.fellipe@live.com>
2025-11-21 15:35:37 +0100

    refact: open bank account command test to use factory to build command in setup

--- test/banking_api/commands/open_bank_account_test.exs
+++ test/banking_api/commands/open_bank_account_test.exs
@@ -3,6 +3,8 @@ defmodule BankingApi.BankAccounts.Commands.OpenBankAccountTest do
 
   alias BankingApi.BankAccounts.Commands.OpenBankAccount
 
+  import BankingApi.CommandsFactory
+
   describe "new/1" do
     test "returns a OpenBankAccount command struct with the given initial values" do
       params = %{"initial_balance" => 1000}
@@ -20,64 +22,41 @@ defmodule BankingApi.BankAccounts.Commands.OpenBankAccountTest do
   end
 
   describe "valid?/1" do
-    @open_bank_account_command %OpenBankAccount{
-      id: Ecto.UUID.generate(),
-      initial_balance: 1000,
-      account_number: "1234567890",
-      status: "open"
-    }
-
     @required_params [:id, :initial_balance, :account_number, :status]
 
     for required_param <- @required_params do
       test "returns false when #{required_param} param is not given" do
-        param = Map.new() |> Map.put_new(unquote(required_param), nil)
-        open_bank_account_command = struct(@open_bank_account_command, param)
-        refute OpenBankAccount.valid?(open_bank_account_command)
+        empty_param = Map.new() |> Map.put_new(unquote(required_param), nil)
+        command = build(:open_bank_account, empty_param)
+        refute OpenBankAccount.valid?(command)
       end
     end
 
     test "returns false when the given id is not UUID" do
-      open_bank_account_command = %OpenBankAccount{
-        @open_bank_account_command
-        | id: "1"
-      }
-
-      refute OpenBankAccount.valid?(open_bank_account_command)
+      command = build(:open_bank_account, id: "1")
+      refute OpenBankAccount.valid?(command)
     end
 
     test "returns false when the given account number is not a string" do
-      open_bank_account_command = %OpenBankAccount{
-        @open_bank_account_command
-        | account_number: 123_456
-      }
-
-      refute OpenBankAccount.valid?(open_bank_account_command)
+      command = build(:open_bank_account, account_number: 123_456)
+      refute OpenBankAccount.valid?(command)
     end
 
     test "returns false when the given initial balance is not greater than 0" do
-      open_bank_account_command = %OpenBankAccount{
-        @open_bank_account_command
-        | initial_balance: -1
-      }
-
-      refute OpenBankAccount.valid?(open_bank_account_command)
+      command = build(:open_bank_account, initial_balance: -1)
+      refute OpenBankAccount.valid?(command)
     end
 
     test "returns true when the given is status is open or closed" do
       for status <- ["open", "closed"] do
-        assert OpenBankAccount.valid?(%OpenBankAccount{
-                 @open_bank_account_command
-                 | status: status
-               })
+        command = build(:open_bank_account, status: status)
+        assert OpenBankAccount.valid?(command)
       end
     end
 
     test "returns false when the given is status is not open or closed" do
-      refute OpenBankAccount.valid?(%OpenBankAccount{
-               @open_bank_account_command
-               | status: "invalid_status"
-             })
+      command = build(:open_bank_account, status: "invalid_status")
+      refute OpenBankAccount.valid?(command)
     end
   end
 end
