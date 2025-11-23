e3b56e0e057c0e9106c0613cfb4edd50684a555f
luan fellipe <luan.fellipe@live.com>
2025-11-23 19:52:37 +0100

    refact: improve test descriptions for close, deposit, and withdraw controllers

--- test/banking_api_web/controllers/api/close_bank_account_controller_test.exs
+++ test/banking_api_web/controllers/api/close_bank_account_controller_test.exs
@@ -5,11 +5,11 @@ defmodule BankingApiWeb.Api.CloseBankAccountControllerTest do
 
   alias BankingApi.BankAccounts.Commands.OpenBankAccount
 
-  @close_attrs %{"account_number" => "0001-01"}
+  describe "POST /api/bank_account/close" do
+    @close_attrs %{"account_number" => "0001-01"}
 
-  describe "POST /api/bank_account/:account_number/close" do
     @tag :web
-    test "successfully close a bank account and respond with 201 status code", %{conn: conn} do
+    test "success: closes a bank account, returns account record", %{conn: conn} do
       dispatch(%OpenBankAccount{}, account_number: @close_attrs["account_number"])
 
       conn = post(conn, ~p"/api/bank_account/close", @close_attrs)
@@ -21,7 +21,7 @@ defmodule BankingApiWeb.Api.CloseBankAccountControllerTest do
     end
 
     @tag :web
-    test "respond with 404 when account with the given number is not found", %{conn: conn} do
+    test "error: returns error when account with the given number is not found", %{conn: conn} do
       invalid_attrs = %{"account_number" => "ACC-NON-EXISTENT"}
 
       conn = post(conn, ~p"/api/bank_account/close", invalid_attrs)
--- test/banking_api_web/controllers/api/deposit_controller_test.exs
+++ test/banking_api_web/controllers/api/deposit_controller_test.exs
@@ -3,11 +3,11 @@ defmodule BankingApiWeb.Api.DepositControllerTest do
 
   alias BankingApi.BankAccounts.Commands.OpenBankAccount
 
-  @create_attrs %{"amount" => 100, "account_number" => "0001-01"}
-
   describe "POST /api/bank_account/deposit" do
+    @create_attrs %{"amount" => 100, "account_number" => "0001-01"}
+
     @tag :web
-    test "successfully deposit when data is valid", %{conn: conn} do
+    test "success: deposits when data is valid", %{conn: conn} do
       dispatch(%OpenBankAccount{}, account_number: @create_attrs["account_number"])
 
       conn = post(conn, ~p"/api/bank_account/deposit", @create_attrs)
@@ -19,7 +19,7 @@ defmodule BankingApiWeb.Api.DepositControllerTest do
     end
 
     @tag :web
-    test "respond with errors when data is invalid", %{conn: conn} do
+    test "error: returns error when data is invalid", %{conn: conn} do
       dispatch(%OpenBankAccount{}, account_number: @create_attrs["account_number"])
 
       invalid_attrs = %{@create_attrs | "amount" => nil}
@@ -30,13 +30,13 @@ defmodule BankingApiWeb.Api.DepositControllerTest do
     end
 
     @tag :web
-    test "respond with 404 when account with the given number is not found", %{conn: conn} do
+    test "error: returns error when account with the given number is not found", %{conn: conn} do
       conn = post(conn, ~p"/api/bank_account/deposit", @create_attrs)
       assert json_response(conn, 404) == %{"error" => "Not found!"}
     end
 
     @tag :web
-    test "respond with 422 when account is already closed", %{conn: conn} do
+    test "error: returns error when account is already closed", %{conn: conn} do
       dispatch(%OpenBankAccount{}, account_number: @create_attrs["account_number"])
 
       dispatch(%BankingApi.BankAccounts.Commands.CloseBankAccount{},
